2ac741f54f8e5e15123595ae8b24c55cf3fbe273
Plamen Petkov <>
2021-03-28 21:53:09 +0300

    Sign in page created. Sign in forms created and styled, currently without the functionality to log in.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component';
-import { Route, Switch } from 'react-router-dom'
 import Header from './components/header/header.component'
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import { Route, Switch } from 'react-router-dom'
 import './App.css';
 
 function App() {
@@ -10,9 +11,9 @@ function App() {
     <div>
       <Header />
       <Switch>
-        {/* <div><HomePage /></div> */}
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component'
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSignUp = () => {
+    return <SignIn />
+}
+
+export default SignInAndSignUp
