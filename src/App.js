ab3b7cb8ceebffbd0790d75217d4c1ace3bd4cea
Plamen Petkov <>
2021-03-28 18:18:25 +0300

    added header component; header component styles in progress

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component';
 import { Route, Switch } from 'react-router-dom'
+import Header from './components/header/header.component'
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         {/* <div><HomePage /></div> */}
         <Route exact path='/' component={HomePage} />
