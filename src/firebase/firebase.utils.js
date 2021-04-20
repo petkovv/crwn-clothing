5c59ed20d096ba4626ea731b95777fa637a54830
Plamen Petkov <>
2021-04-20 20:34:16 +0300

    Implemented loading spinner, Redux Thunk and container pattern

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,40 +3,73 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyCXJnrhfwIvaqfb2QaPlHOtDjFFmpy4sOM",
-    authDomain: "crwn-db-f99ad.firebaseapp.com",
-    projectId: "crwn-db-f99ad",
-    storageBucket: "crwn-db-f99ad.appspot.com",
-    messagingSenderId: "846995458870",
-    appId: "1:846995458870:web:9187f865dbcc8db12bc197",
-    measurementId: "G-XHEPHS25L0"
+  apiKey: "AIzaSyCXJnrhfwIvaqfb2QaPlHOtDjFFmpy4sOM",
+  authDomain: "crwn-db-f99ad.firebaseapp.com",
+  projectId: "crwn-db-f99ad",
+  storageBucket: "crwn-db-f99ad.appspot.com",
+  messagingSenderId: "846995458870",
+  appId: "1:846995458870:web:9187f865dbcc8db12bc197",
+  measurementId: "G-XHEPHS25L0"
 };
 
 firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if (!userAuth) return;
+  if (!userAuth) return;
 
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-    const snapShot = await userRef.get();
+  const snapShot = await userRef.get();
 
-    if (!snapShot.exists) {
-        const { displayName, email } = userAuth;
-        const createdAt = new Date();
-        try {
-            await userRef.set({
-                displayName,
-                email,
-                createdAt,
-                ...additionalData
-            });
-        } catch (error) {
-            console.log('error creating user', error.message);
-        }
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
     }
+  }
 
-    return userRef;
+  return userRef;
+};
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
 };
 
 export const auth = firebase.auth();
